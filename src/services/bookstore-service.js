41f60233d7cda2a37e7dfa12537a818a178b1464
OveR1st <315overdose@gmail.com>
2020-12-15 13:47:31 +0200

    реакт оберертки для App

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,9 +1,19 @@
-import React from 'react';
+// import React from 'react';
 
 
 export default class BookstoreService {
   
   getBooks() {
-    return [];
+    return [
+      { 
+        id: 1,
+        title: 'Production-Ready Microservices',
+        author: 'Susan J. Fowler' },
+
+      { 
+        id: 2,
+        title: 'Release It!',
+        author: 'Michael T. Nygard' }
+    ];
   };
 };
